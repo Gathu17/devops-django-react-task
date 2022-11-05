4a2db77cc901b0bbdf5caa85d2e5dc1286f339e3
jayGathu <jjgathu17@gmail.com>
2022-11-05 13:16:24 +0300

    commit

---
+++ api/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\GATHU\Desktop\DevOps task\devops-django-react-task\api\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
